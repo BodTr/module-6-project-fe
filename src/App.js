87f6952a3ec1cb69fdbe9686ca2a7a60e9597c5c
BodTr <trung150499@gmail.com>
2024-12-19 10:05:01 +0700

    add song player

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import { CSpinner, useColorModes } from '@coreui/react';
+import 'react-h5-audio-player/src/styles.scss';
 import './scss/style.scss';
 
 // We use those styles to show code examples, you should remove them in your application.
 import './scss/examples.scss';
+
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
